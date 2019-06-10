dbbf3997baa78fdb31695089c0af9d38fd27f25c
Henri Korhonen <henri.s.korhonen@gmail.com>
2019-06-10 14:19:06 +0300

    Added csrf protection + form to create new users

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
 
 
 db = SQLAlchemy()
+csrf = CSRFProtect()
 
 
 def create_app():
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object('config.Config')
     db.init_app(app)
+    csrf.init_app(app)
 
     with app.app_context():
         from . import routes
@@ -16,3 +19,9 @@ def create_app():
         db.create_all()
 
         return app
+
+
+# TODO: easy way to create and edit accounts + selecting payment
+# TODO: Token buy view
+# TODO: User token wallet
+# TODO: mock up Authorization of users in news sites
--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,39 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, request
 from flask import current_app as app
 from .models import db, User
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
 
 
-@app.route('/new', methods=['GET'])
-def entry():
+class NewUserForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+    email = StringField('email', validators=[DataRequired()])
+
+
+@app.route('/new', methods=['GET', 'POST'])
+def new_entry():
     """Endpoint to create a user."""
-    new_user = User(username='test',
-                    email='test@example.com',
-                    )
-    try:
-        db.session.add(new_user)
-        db.session.commit()
-    except:
-        pass
+    form = NewUserForm()
+    if form.validate_on_submit():
+        try:
+            new_user = User(username=request.form.get('name'), email=request.form.get('email'))
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for('index'))
+        except Exception as e:
+            print(e)
+            return render_template('new_user.html', form=form)
+    return render_template('new_user.html', form=form)
+
+
+@app.route('/users')
+def users():
+    """Lists all users"""
     users = User.query.all()
     return render_template('users.html', users=users, title="Show Users")
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
