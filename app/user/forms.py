a87cbe53289fd97548d5439be7e77f5fc896f7a0
Henri Korhonen <henri.s.korhonen@gmail.com>
2019-06-18 15:58:54 +0300

    Edit user stuff

--- app/user/forms.py
+++ app/user/forms.py
@@ -1,10 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import RadioField, StringField
-from wtforms.validators import DataRequired
+from wtforms import PasswordField, StringField
 
 
 class Edit(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
-    email = StringField('email', validators=[DataRequired()])
-    payment_method = RadioField('Payment method', choices=[(0, 'Single'), (1, 'Monthly')],
-                                default=0, validators=[DataRequired()], coerce=int)
+    name = StringField('name')
+    email = StringField('email')
+    password = PasswordField('password')
--- app/user/views.py
+++ app/user/views.py
@@ -1,7 +1,10 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import current_user, login_required
-from .forms import Edit
 from datetime import date
+from passlib.hash import pbkdf2_sha256
+from app.db import db
+from .forms import Edit
+
 bp = Blueprint('user', __name__)
 
 
@@ -9,10 +12,18 @@ bp = Blueprint('user', __name__)
 @login_required
 def edit():
     form = Edit()
-    print(current_user)
+    if form.validate_on_submit():
+        if form.name.data:
+            current_user.username = form.name.data
+        if form.email.data:
+            current_user.email = form.email.data
+        if form.password.data:
+            current_user.password = pbkdf2_sha256.hash(form.password.data)
+        db.session.commit()
+        return redirect(url_for('index'))
+
     form.name.data = current_user.username
     form.email.data = current_user.email
-    form.payment_method.data = int(current_user.monthly_pay)
     return render_template('user/edit_user.html', form=form)
 
 
@@ -20,12 +31,13 @@ def edit():
 @login_required
 def pay():
     payment_button = False
-    print(current_user.subscription_end)
-    print(type(current_user.subscription_end))
-    if current_user.monthly_pay:
-        if current_user.subscription_end is None:
-            payment_button = True
-        elif current_user.subscription_end <= date.today():
-            payment_button = True
+    subs = False
+    if current_user.subscription_end is None:
+        payment_button = True
+    elif current_user.subscription_end <= date.today():
+        payment_button = True
 
-    return render_template('user/pay_month.html', button=payment_button)
+    if current_user.subscription_end is not None:
+        subs = True
+
+    return render_template('user/pay_month.html', button=payment_button, subs=subs)
