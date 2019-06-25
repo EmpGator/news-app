7f2eb34427bd1704a14c3d0b7c46ed725b7a00d4
Henri Korhonen <henri.s.korhonen@gmail.com>
2019-06-25 12:46:02 +0300

    Minor fix

--- app/user/views.py
+++ app/user/views.py
@@ -52,9 +52,9 @@ def profile():
     name = current_user.first_name + ' ' + current_user.last_name
     email = current_user.email
     bought = pickle.loads(current_user.paid_articles)
-    end = current_user.subscription_end
+    end = str(current_user.subscription_end) if current_user.subscription_end else None
     paid = current_user.prepaid_articles
-    data = {'name': name, 'email': email, 'bought': bought, 'end_date': str(end),
+    data = {'name': name, 'email': email, 'bought': bought, 'end_date': end,
             'prepaid': paid}
     data = json.dumps(data)
     return render_template('index.html', data=data)
