06a9afbded4e8035be7156d01599a0aa7d46abe0
Henri Korhonen <henri.s.korhonen@gmail.com>
2019-07-11 17:09:36 +0300

    some form validation added

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,11 @@
-from flask import Blueprint, render_template, redirect, url_for, request, flash, make_response, session
+import re
+
+from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_jwt_extended import create_access_token
 from flask_login import current_user, login_user, logout_user, login_required
 
-from app.constants import PUBLISHER_DOMAIN
+
+from app.constants import PUBLISHER_DOMAIN, BAD_CHAR_LIST
 from app.models import User
 from app.db import db
 from passlib.hash import pbkdf2_sha256
@@ -10,6 +13,30 @@ from app.constants import Role
 
 
 bp = Blueprint('auth', __name__)
+
+# TODO: move validation functions to own file maybe
+def validate_email(email):
+    if not re.match('[^@]+@[^@]+\.[^@]+', email):
+        flash('Possibly bad email format')
+        raise Exception('Possibly bad email format')
+    user = User.query.filter_by(email=email).first()
+    if user:
+        flash('Email address is taken')
+        raise Exception('Email is taken')
+
+
+def validate_name(name):
+    if any(map(lambda i: i in name, BAD_CHAR_LIST)):
+        flash('Bad character in name')
+        raise Exception('bad character in name')
+
+
+def validate_and_hash_password(pw, pw_again):
+    if pw != pw_again:
+        flash('passwords are different')
+        raise Exception('different passwords')
+    pw_hash = pbkdf2_sha256.hash(pw)
+    return pw_hash
 
 
 @bp.route('/signup', methods=['GET', 'POST'])
@@ -23,15 +50,19 @@ def new_entry():
         fn = request.form.get('firstName')
         ln = request.form.get('lastName')
         email = request.form.get('email')
-        pw_hash = pbkdf2_sha256.hash(request.form.get('password'))
+        pw = request.form.get('password')
+        pw_again = request.form.get('password')
         try:
+            validate_name(fn+ln)
+            validate_email(email)
+            pw_hash = validate_and_hash_password(pw, pw_again)
             # noinspection PyArgumentList
             new_user = User(first_name=fn, last_name=ln, email=email, password=pw_hash, role=Role.USER)
             db.session.add(new_user)
             db.session.commit()
+
         except Exception as e:
             print(e)
-            flash('email is taken')
             return redirect(url_for('auth.new_entry'))
         return redirect(url_for('auth.login'))
 
