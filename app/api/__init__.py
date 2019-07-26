77cafa31cb2e4e59cdf61bed3866e06cd529f088
Henri Korhonen <henri.s.korhonen@gmail.com>
2019-07-26 16:20:39 +0300

    major changes and bug fixes to api

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -4,7 +4,7 @@ from flask_restful import Api, Resource
 from flask_restful.reqparse import RequestParser
 from flask_login import current_user, login_required
 from app.db import db
-from app.models import Article, Publisher, User
+from app.models import Article, Publisher, User, ReadArticleLink
 from datetime import date, timedelta
 from app.constants import *
 import requests
@@ -13,95 +13,51 @@ import time
 u_data_req_parser = RequestParser(bundle_errors=True)
 u_data_req_parser.add_argument("url", required=True)
 
-pay_req_parser = RequestParser(bundle_errors=True)
-pay_req_parser.add_argument("url", required=True)
-
 token_req_parser = RequestParser(bundle_errors=True)
 token_req_parser.add_argument('amount', required=True)
+
+# above should be obsolette
+
+pay_req_parser = RequestParser(bundle_errors=True)
+pay_req_parser.add_argument('url', required=True)
+pay_req_parser.add_argument('domain', required=True)
+pay_req_parser.add_argument('article_price')
+pay_req_parser.add_argument('article_name')
+pay_req_parser.add_argument('article_desc')
+pay_req_parser.add_argument('article_date')
+pay_req_parser.add_argument('article_category')
 
 access_req_parser = RequestParser(bundle_errors=True)
 access_req_parser.add_argument('url')
 access_req_parser.add_argument('domain')
+access_req_parser.add_argument('article_name')
+access_req_parser.add_argument('article_desc')
+access_req_parser.add_argument('article_date')
+access_req_parser.add_argument('article_category')
 
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
 
 
-def validate_txid(txid, price):
-    """
-    Uses bitcoin api to validate slp transaction,
-    checks if correct amount was sent to correct address
-
-    :param txid: transaction id
-
-    :param price: amount of tokens that were supposed to be received
-
-    :return: True if transaction was valid Else False
-
-    """
-    print('validate_txid')
-    url = 'https://rest.bitcoin.com/v2/slp/txDetails/' + txid
-    resp = requests.get(url)
-    retries = 5
-    output_n = None
-    tokens_received = 0
-    for i in range(retries):
-        if resp.status_code == 200:
-            data = resp.json()
-            for i in data['vout']:
-                slp = i['scriptPubKey'].get('slpAddrs', [])
-                if SLP_ADDR in slp:
-                    output_n = i['n']
-                    break
-            if output_n:
-                tokens_received = int(data['tokenInfo']['sendOutputs'][output_n])
-            if tokens_received >= price:
-                print('return true')
-                return True
-        else:
-            print(resp.status_code)
-            print(resp.text)
-            time.sleep(5)
-    print('return false')
-    return False
-
-
-def get_article(url):
+def get_article(url, domain=None, art_name='', art_date=date.today(), art_desc='', art_category=None):
     """
     Fetches article from database, if article wasn't found, creates new article object
 
     :param url: Article url, used to fetch article from database
+    :param domain:
 
     :return: article object
     """
     article = Article.query.filter_by(url=url).first()
     if not article:
-        split_url = url.split('/')
-        pub_name = 'mock'
-        art_name = 'placeholder'
-        if len(split_url) >= 3:
-            if split_url[3] == 'ts':
-                pub_name = 'Turun sanomat'
-            elif split_url[3] == 'hs':
-                pub_name = 'Helsingin sanomat'
-            elif split_url[3] == 'ks':
-                pub_name = 'Keskisuomalainen'
-            elif split_url[3] == 'ss':
-                pub_name = 'Savon sanomat'
-            elif split_url[3] == 'kl':
-                pub_name = 'Kauppalehti'
-        # This is kinda bad way to get name for publisher
-        # Consider adding domain/base url for published model and
-        # Querying publisher directly with that
-            art_name = ' '.join(split_url[3:])
-            # Ok this makes basically no sense. Either go fetch title or use rss feed generated objects
+        publisher = Publisher.query.filter_by(url=domain).first()
+        if not publisher:
+            publisher = Publisher.query.filter_by(name='mock').first()
 
-        publisher = Publisher.query.filter_by(name=pub_name).first()
-        from random import randint
-        from app.constants import Category
-        cat = Category(randint(1, 6))
-        article = Article(url=url, publisher=publisher, name=art_name, category=cat)
+        article = Article(url=url, publisher=publisher, name=art_name, category=art_category,
+                          date=art_date, description=art_desc)
+
         db.session.add(article)
         db.session.commit()
     return article
@@ -122,9 +78,48 @@ def get_authenticated_user(user):
         return User.query.get(int(uid))
 
 
+def get_user_info(user):
+    if user.subscription_end:
+        method = 'Monthly Subscription'
+        msg = f'Valid until {user.subscription_end}'
+    elif user.prepaid_articles:
+        method = 'Package subscription'
+        msg = f'{user.prepaid_articles} articles left'
+    else:
+        method = 'Single payments'
+        msg = f'{user.tokens} tokens left'
+
+    data = {
+        'access': False,
+        'name': user.first_name,
+        'method': method,
+        'expiration': user.subscription_end,
+        'package_left': user.prepaid_articles,
+        'tokens_left': user.tokens,
+        'message': msg
+    }
+    return data
 
 
-class Userdata(Resource):
+def get_article_from_args(args):
+    url = args.get('url')
+    article = Article.query.filter_by(url=url).first()
+    if not article and all(args.values()):
+        domain = args.get('domain')
+        name = args.get('article_name')
+        desc = args.get('article_desc')
+        day = args.get('article_date')
+        category = args.get('article_category')
+        day = date.fromisoformat(day)
+        category = Category(category)
+        article = get_article(url, domain=domain, art_name=name, art_desc=desc,
+                              art_category=category, art_date=day)
+    return article
+
+
+
+
+class OldUserdata(Resource):
     """
     Handles user access data to give url
 
@@ -147,6 +142,7 @@ class Userdata(Resource):
         elif current_user.subscription_end is not None:
             if current_user.subscription_end >= date.today():
                 if article not in current_user.read_articles:
+                    # TODO: change this to current_user.read_articles.append(article_link)
                     current_user.read_articles.append(article)
                     article.hits += 1
                     article.monthly_pay += 1
@@ -174,45 +170,6 @@ class Userdata(Resource):
 class OldPaidArticle(Resource):
     """
     Handles token payment POST requests from publishers
-    """
-    @jwt_optional
-    def post(self):
-        """
-        Handles post request
-
-        possible optimization: add access and user information to response as well
-
-        :return: Response object with text OK or Not enough Tokens
-        """
-        local_user = current_user
-        if not local_user.is_authenticated:
-            uid = get_jwt_identity()
-            if uid:
-                local_user = User.query.get(int(uid))
-            else:
-                return make_response('Bad username or password', 403)
-        args = pay_req_parser.parse_args()
-        url = args['url']
-        if local_user.tokens > 0:
-            analytics = Publisher.query.filter_by(name='All').first()
-            article = get_article(url)
-            article.hits += 1
-            article.single_pay += 1
-            article.publisher.single_pay += 1
-            article.publisher.revenue += 1
-            analytics.revenue += 1
-            analytics.single_pay += 1
-            local_user.articles.append(article)
-            local_user.tokens -= 1
-            db.session.commit()
-            resp = make_response('Ok', 200)
-            return resp
-        return make_response('Not enough tokens', 200)
-
-
-class PaidArticle(Resource):
-    """
-    Handles token payment POST requests from publishers
     TODO: make this handle all payment methods
     """
     @jwt_optional
@@ -234,36 +191,158 @@ class PaidArticle(Resource):
         return make_response('Not enough tokens', 200)
 
 
-class Test(Resource):
+class UserInfo(Resource):
     """
     Test for unified endpoint for article pay, user info and accessdata
     """
     @jwt_optional
     def post(self):
         """
-        TODO: Make this only return access information and userdata
+        This sends user information,
+        all arguments are optional, but if provided, can be used to tell
+        if user has access given article or not.
+
+        request body can be left empty or can contain all relevant article information. If given
+        article info, new article will be made and added to database and. Example request bodies:
+
+
+        Request:
+            { "url": "http://localhost:8000/ts/article/1" }
+
+
+        Response:
+            {
+                "access": false,
+                "can_pay": true,
+                "name": "Matti",
+                "method": "Single payments",
+                "expiration": null,
+                "package_left": 0,
+                "tokens_left": 23,
+                "message": "23 Tokens left"
+            }
+
+
+        Request:
+            {
+                "url": "http://localhost:8000/ts/article/1",
+                "domain": "http://localhost:8000/ts/",
+                "article_name": "Kahvi on hyvää",
+                "article_desc": "Kahvi on hyvää ja terveellistä",
+                "article_date": "2019-06-21",
+                "article_category": "health"
+            }
+
+        Response:
+            {
+                "access": true,
+                "can_pay": true,
+                "name": "Matti",
+                "method": "Package subscription",
+                "expiration": null,
+                "package_left": 8,
+                "tokens_left": 103,
+                "message": "8 prepaid articles left"
+            }
+
+        article_date must be format YYYY-MM-DD
+        currently valid categories are:
+            'politics'
+            'sports'
+            'economy'
+            'technology'
+            'health'
+            'entertainment'
+
+
         :return:
         """
         user = get_authenticated_user(current_user)
         if not user:
             return make_response('bad auth', 403)
+
         args = access_req_parser.parse_args()
-        url = args.get('url')
-        article = get_article(url)
-        data = {
-            'access': False,
-            'name': user.first_name,
-            'method': None,
-            'expiration': user.subscription_end,
-            'package_left': user.prepaid_articles,
-            'tokens_left': user.tokens,
-            'message': 'ok'
-        }
+        data = get_user_info(user)
+        article = get_article_from_args(args)
+        data['can_pay'] = False
         if article:
             data['access'] = user.access_article(article)
+        if user.can_pay():
+            data['can_pay'] = True
         return jsonify(data)
 
 
+class PayArticle(Resource):
+    """
+    testpay
+    """
+    @jwt_optional
+    def post(self):
+        """
+        Handles attempted payment of article.
+
+        This attempts to pay given in request body. If article does not exist it attempts to create
+        new article object and add it to database. It responds with user information and if payment
+        was successful. Url and domain are required attributes, but if given article doesnt exist
+        payment can't happen so article information should be provided as well. Example
+        request/response bodies:
+
+        Request:
+            { "url": "http://localhost:8000/ts/article/1", "domain": "http://localhost:8000/ts" }
+
+
+        Response:
+            {
+                "payment_successful": true,
+                "access": true,
+                "name": "Matti",
+                "method": "Single payments",
+                "expiration": null,
+                "package_left": 0,
+                "tokens_left": 23,
+                "message": "23 Tokens left"
+            }
+
+
+        Request:
+            {
+                "url": "http://localhost:8000/ts/article/1",
+                "domain": "http://localhost:8000/ts/",
+                "article_name": "Kahvi on hyvää",
+                "article_desc": "Kahvi on hyvää ja terveellistä",
+                "article_date": "2019-06-21",
+                "article_category": "health",
+                "article_price": 2
+            }
+
+        Response:
+            {
+                "payment_successful": true,
+                "access": true,
+                "name": "Matti",
+                "method": "Package subscription",
+                "expiration": null,
+                "package_left": 8,
+                "tokens_left": 103,
+                "message": "8 prepaid articles left"
+            }
+
+
+        :return:
+        """
+        user = get_authenticated_user(current_user)
+        if not user:
+            return make_response('bad auth', 403)
+        args = pay_req_parser.parse_args()
+        price = args.get('article_price')
+        article = get_article_from_args(args)
+        data = get_user_info(user)
+        data['payment_successful'] = True
+        if article and user.pay_article(article, price=price):
+            data['access'] = True
+            data['payment_successful'] = True
+
+        return jsonify(data)
 
 
 
@@ -276,9 +355,10 @@ class TopUp(Resource):
         """
         Handles POST request
         Checks chosen package and if amount was given. Adds information to user object
-
+        TODO: move this to user module
         :return: Redirect to index page
         """
+        print(request.form)
         option = PayOptions(request.form.get('pay-method', "0"))
         if option == PayOptions.MONTHLY:
             analytics = Publisher.query.filter_by(name='All').first()
@@ -307,7 +387,7 @@ class TopUp(Resource):
         return redirect(url_for('index'))
 
 
-class Userinfo(Resource):
+class OldUserinfo(Resource):
     """
     Class that sends relevant information of user
     """
@@ -337,8 +417,11 @@ class Userinfo(Resource):
         data = {'name': local_user.first_name, 'payment_type': pay, 'value': value}
         return jsonify(data)
 
-api.add_resource(Userdata, '/api/userdata')
-api.add_resource(PaidArticle, '/api/articlepaid')
-api.add_resource(TopUp, '/api/topup')
-api.add_resource(Userinfo, '/api/userinfo')
-api.add_resource(Test, '/api/test')
+api.add_resource(OldUserdata, '/api/userdata')
+api.add_resource(OldPaidArticle, '/api/articlepaid')
+api.add_resource(OldUserinfo, '/api/olduserinfo')
+# above routes should be obsolette
+api.add_resource(TopUp, '/topup')
+# Above should be moved
+api.add_resource(PayArticle, '/api/payarticle')
+api.add_resource(UserInfo, '/api/userinfo')
