7d36ca186a7844d56e82275bf3a40b7261197051
Henri Korhonen <henri.s.korhonen@gmail.com>
2020-04-08 19:04:24 +0300

    Thumbnails fixed

--- app/views.py
+++ app/views.py
@@ -115,6 +115,17 @@ def fetch_articles_view():
     return make_response('ok', 200)
 
 
+@app.route('/temp')
+def rename_db_images():
+    from app.models import Article
+    articles = Article.query.all()
+    for article in articles:
+        article.image = f'static/thumbnails/thumbnail_{article.id}.jpg'
+        print(article.image)
+    db.session.commit()
+    return 'ok'
+
+
 @app.route('/')
 def index():
     """
