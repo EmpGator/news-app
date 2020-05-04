5078bdff0e3eb0bf714d65fc03ee3ac0f4ffae8a
Henri Korhonen <henri.s.korhonen@gmail.com>
2020-05-04 17:35:45 +0300

    Auto article parse from rss

--- app/views.py
+++ app/views.py
@@ -1,11 +1,13 @@
 from datetime import datetime, date
 from time import time
-
+from bs4 import BeautifulSoup
 import feedparser
+import requests
 from flask import render_template, redirect, url_for, request, make_response, jsonify
 from flask import current_app as app
 from flask_jwt_extended import create_access_token
 from flask_login import login_required, current_user
+from sqlalchemy import desc
 from werkzeug.useragents import UserAgent
 
 from app.constants import PUBLISHER_DOMAIN, Role, Category
@@ -25,13 +27,21 @@ def get_articles(publishers=None, categories=None):
     :return: article data in python dictionary
 
     """
+    MAX_PER_PUB = 15
     data = []
     for cat in Category:
-        articles = Article.query.filter(Article.category == cat)
+        articles = Article.query.filter(Article.category == cat).order_by(desc(Article.date))
         art_data_lst = []
+        amount_from_pub = {}
         for i, entry in enumerate(articles):
-            art_data = get_article_data(entry)
-            art_data_lst.append(art_data)
+            try:
+                amount_from_pub[entry.publisher] += 1
+            except KeyError:
+                amount_from_pub[entry.publisher] = 1
+            if amount_from_pub[entry.publisher] <= MAX_PER_PUB:
+                art_data = get_article_data(entry)
+                art_data_lst.append(art_data)
+
         headline = cat.value
         data.append(dict(name=headline.title(), content=art_data_lst))
     return data
@@ -57,52 +67,111 @@ def get_article_data(article):
 
 def fetch_articles():
     print('Fetching articles')
-    url_list = [(i.rss, i.url) for i in Publisher.query.filter(Publisher.name != 'All')]
-    for src, url in url_list:
+    publishers = Publisher.query.filter(Publisher.name != 'All')
+    articles_url_association_dict = {}
+    articles = Article.query.all()
+    external_publishers = [
+        'theguardian.com',
+        'politico.com',
+        'theverge.com',
+        'engadget.com',
+        'usatoday'
+    ]
+    for publisher in publishers:
+        src = publisher.rss
+        url = publisher.url
         print(f'rss: {src}, url: {url}')
         feed = feedparser.parse(src)
-        author = Publisher.query.filter_by(url=url).first()
+        author = publisher
         if author:
-            for i, entry in enumerate(feed.entries):
-                url = entry.link
-                existing_art = Article.query.filter_by(url=url).first()
-
-                try:
-                    img = entry.media_content[0]['url']
-                except Exception as e:
-                    print(e)
-                    img = None
-                if not img:
-                    img = author.image
-                try:
-                    category = Category(entry.category)
-                except Exception as e:
-                    category = Category('')
-                try:
-                    day = date.fromisoformat(entry.published)
-                except Exception as e:
-                    day = date.today()
-                if not existing_art:
-                    article = Article(name=entry.title, publisher=author, image=img, url=url,
-                                      description=entry.description, date=day, category=category)
-                    db.session.add(article)
-                    db.session.commit()
-                else:
-                    existing_art.image = img
-                    existing_art.category = category
-                    existing_art.date = day
-                    db.session.commit()
-
-
-            print(f'fetched {author.url} articles succesfully')
+            try:
+                for i, entry in enumerate(feed.entries):
+                    fetch_from_mocksite = False
+                    existing = False
+                    url = entry.link
+                    if any([i in url for i in external_publishers]):
+                        articles_url_association_dict[url] = None
+                        fetch_from_mocksite = True
+                    article = [i for i in articles if i.url == url] or None
+                    if article:
+                        article = article[0]
+                    try:
+                        img = entry.media_content[0]['url']
+                    except Exception as e:
+                        img = None
+                    if not img:
+                        img = author.image
+                    try:
+                        terms = [i.term.lower() for i in entry.tags]
+                        # TODO: coerce wordlist entries into category
+                        for c in Category:
+                            if c.value in terms:
+                                category = c
+                                break
+                        #category = Category(entry.category)
+                        if 'politico' in url:
+                            category = Category.HEALTH
+                    except Exception as e:
+                        category = Category('')
+                    try:
+                        day = date.fromisoformat(entry.published)
+                    except Exception as e:
+                        day = date.today()
+                    try:
+                        desc = BeautifulSoup(entry.description, "lxml").text
+                    except:
+                        if hasattr(entry, 'description'):
+                            desc = entry.description
+                            soup = BeautifulSoup(desc)
+                            desc = soup.text.strip()
+                            for img in soup.findAll('img'):
+                                img = img.get('src')
+                                break
+                    if not article:
+                        article = Article(name=entry.title, publisher=author, image=img, url=url,
+                                          description=desc, date=day, category=category)
+                    else:
+                        existing = True
+                        article.image = img if not article.image else article.image
+                        article.category = category if category else article.category
+                        article.date = day if not article.date else article.date
+                        if entry.description:
+                            article.description = entry.description
+                    if fetch_from_mocksite:
+                        articles_url_association_dict[url] = article
+                    if not existing and not fetch_from_mocksite:
+                        db.session.add(article)
+                print(f'fetched {author.url} articles succesfully')
+            except Exception as e:
+                print(e)
         else:
             print('Couldnt find author with given url')
             print(f'URL:\n{url}')
             print('\n'*3)
-            publishers = [i.url for i in Publisher.query.all()]
             for i in publishers:
                 print(i)
             print('\n'*3)
+    try:
+        payload = {'urls': list(articles_url_association_dict.keys())}
+        url = f'http://{PUBLISHER_DOMAIN}/check_urls'
+        res = requests.post(url, json=payload)
+        data = res.json()
+        urls = [i.url for i in articles]
+
+        for url, article in articles_url_association_dict.items():
+            mock_url = data.get(url, None)
+            if mock_url and mock_url not in urls:
+                article.url = mock_url
+                db.session.add(article)
+            else:
+                print(article.url)
+                try:
+                    db.session.expunge(article)
+                except Exception as e:
+                    print(f'Exception: {e}')
+    except Exception as e:
+        print(e)
+    db.session.commit()
 
 @app.route('/fetch_articles')
 def fetch_articles_view():
